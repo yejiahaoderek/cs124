8bf9cbfcdcfed0c35d02e1220f1ecd5a3221fbdb
yejiahao <jyab2020@mymail.pomona.edu>
2021-09-19 22:20:42 -0700

    created base code

--- app/src/index.js
+++ app/src/index.js
@@ -5,13 +5,30 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+        <h1>To-Dos</h1>
+        <ul>
+            <li>Uncompleted Task 1</li>
+            <li>Uncompleted Task 2</li>
+        </ul>
+
+        <button>Hide Completed</button>
+        <h1>Completed Tasks</h1>
+        <button>Delete All</button>
+        <ul>
+            <li>Completed Task 1</li>
+            <li>Completed Task 2</li>
+        </ul>
+
+        <form>
+            <label>
+                Enter your new task here
+                <input type="text" name="task" />
+            </label>
+            <button>
+                Confirm
+            </button>
+        </form>
+    </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
